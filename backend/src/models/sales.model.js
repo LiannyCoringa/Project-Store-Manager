51e16090962c5e6029634372399d22776164e051
Lianny Coringa <lianny_costa@hotmail.com>
2023-10-19 20:23:09 -0300

    Tentando ajeitar o erro

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -1,24 +1,23 @@
-const camelize = require('camelize');
 const connection = require('./connection');
 
 const findModel = async () => {
   const [sales] = await connection.execute(`SELECT
-  sp.sale_id, s.date, sp.product_id, sp.quantity
+  sp.sale_id AS saleId, s.date AS date, sp.product_id AS productId, sp.quantity AS quantity
   FROM sales_products AS sp
   INNER JOIN sales AS s
   ON s.id = sp.sale_id
   ORDER BY sp.sale_id ASC, sp.product_id ASC;`);
-  return camelize(sales);
+  return sales;
 };
 const findIdModel = async (id) => {
   const [sales] = await connection.execute(`SELECT
-  s.date, sp.product_id, sp.quantity
+  s.date AS date, sp.product_id AS productId, sp.quantity AS quantity
   FROM sales AS s
   INNER JOIN sales_products AS sp
   ON s.id = sp.sale_id
   WHERE s.id = ?
   ORDER BY sp.product_id ASC;`, [id]);
-  return camelize(sales);
+  return sales;
 };
 
 module.exports = {
